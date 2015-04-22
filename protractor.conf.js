326a944753999d20c99e2bdac8de703f08897bef
Marnen Laibow-Koser <marnen@marnen.org>
2015-04-22 14:15:39 -0400

    Set up Protractor for PhantomJS.

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,7 +4,19 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'phantomjs', // may cause problems, see https://github.com/angular/protractor/blob/master/docs/browser-setup.md#setting-up-phantomjs
+
+    /*
+     * Can be used to specify the phantomjs binary path.
+     * This can generally be ommitted if you installed phantomjs globally.
+     */
+    // 'phantomjs.binary.path': require('phantomjs').path,
+
+    /*
+     * Command line args to pass to ghostdriver, phantomjs's browser driver.
+     * See https://github.com/detro/ghostdriver#faq
+     */
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   },
 
   //seleniumAddress: 'http://0.0.0.0:4444',
